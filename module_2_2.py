c173cdc968c3d264fcca59b4a1ee15a5e81451ed
voinoffvv <voinoffvv@gmail.com>
2024-08-23 14:13:46 +0300

    module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,5 +1,13 @@
 numbers = [0]*3
 for i in range(3):
     numbers[i] = input(f'Введите {i+1} число: ')
+    # если ввели значение, которое нельзя преобраховать в ЦЕЛОЕ, то меняем на 0
+    # поскольку в условии "3 целых числа", то приводим к целому
+    try:
+        numbers[i] = int(numbers[i])
+    except:
+        numbers[i] = 0
+
 res = len(numbers) - len(set(numbers))
+print(numbers)
 print('Количество одинаковых чисел:', (res if res==0 else res+1))
