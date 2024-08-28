b47d9941df6124406c23a02e690cf08c8b61917e
voinoffvv <voinoffvv@gmail.com>
2024-08-28 15:28:31 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,11 +1,12 @@
 def single_root_words(root_word = '', *other_words):
-    res = list()
-    for w in other_words:
-        print(w)
-    return res
+    same_words  = list()
+    for other_word in other_words:
+        if root_word.lower() in other_word.lower() or other_word.lower() in root_word.lower():
+            same_words.append(other_word)
+    return same_words
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-#print(result1)
-#print(result2)
+print(result1)
+print(result2)
