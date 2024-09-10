2548cf14d07b400fdf450fc0b82e33a91ded039d
Вячеслав Воинов <voinoffvv@gmail.com>
2024-09-10 12:30:56 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,34 +1,44 @@
-class Horse:
-    # x_distance = 0
-    # sound = 'Frrr'
+class Horse():
+
     def __init__(self, x_distance = 0,    sound = 'Frrr'):
+        super().__init__()
         self.x_distance = x_distance
         self.sound = sound
     def run(self, dx):
         self.x_distance  += dx
 
     pass
-class Eagle:
+class Eagle():
     # y_distance = 0
     # sound = 'I train, eat, sleep, and repeat'
     def __init__(self, y_distance = 0, sound = 'I train, eat, sleep, and repeat'):
+        super().__init__()
         self.y_distance = y_distance
         self.sound = sound
     def fly(self, dy):
         self.y_distance += dy
 
 class Pegasus(Horse, Eagle):
-    # def __init__(self):
-    #     self.x_distance = super().x_distance
-    #     self.y_distance = super().y_distance
-    #     self.sound = Eagle.sound
-    # def move(self, dx, dy):
-    #     self.run(dx)
-    #     self.fly(dy)
-    # def get_pos(self):
-    #     return (super().x_distance, super().y_distance)
-    # def voice(self):
-    #     print(super().sound)
+    def __init__(self):
+        super().__init__()
+        Eagle.__init__(self)
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+    def voice(self):
+        print(self.sound)
+
+
 
 p1 = Pegasus()
 
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
