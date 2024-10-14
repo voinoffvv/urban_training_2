600edb770d8c4364a303123fa9dddc15b9a817c8
Вячеслав Воинов <voinoffvv@gmail.com>
2024-10-14 12:52:32 +0300

    module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -2,14 +2,12 @@
 # Напишите функцию-генератор all_variants(text), которая принимает строку text и возвращает объект-генератор,
 # при каждой итерации которого будет возвращаться подпоследовательности переданной строки.
 def all_variants(text):
-    for j in range(len(text)):
-        for i in range(len(text)):
+    for i in range(len(text)):
+        for j in range(i, len(text)):
             c = text[i: j + 1:]
-            if c != '':
-                yield c
+            yield c
 
-
-a = all_variants("abc")
-for i in a:
+for i in all_variants("abc"):
     print(i)
+
 
